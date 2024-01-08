7ba8d9edc4810f56fef923901fe5ac80e00c723a
Leo Chung <leo.chung97@gmail.com>
2024-01-08 11:46:51 -0500

    Initial commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Leo Chung on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
