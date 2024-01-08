c0733737be3b0b843287634345c70da6e085264b
Leo Chung <leo.chung97@gmail.com>
2024-01-08 16:49:01 -0500

    Project 10 Wrap Up

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -8,10 +8,70 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
     var order: Order
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        // reqres.in is a website that lets you send any data you want - it will automatically send it back as well
+        // We force unwrap the URL(string:) initializer which means: "this returns an optional URL, but please force it to be non-optional"
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        // We are sending a POST request since we are writing new data
+        // The content type is specified for the way the server will treat our data - this is called a MIME type
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed: \(error.localizedDescription)")
+        }
+    }
 
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+
+                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+
+                // You can't use the task() modifier here as we are executing an action - instead we can create a new task using Task and add our await call inside
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                    .padding()
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+        // When your views fit on a single screen, they typically bounce a little when the user moves up or down with them
+        // This can be annoying - you can disable by adjusting the scrollBounceBehavior() modifier
     }
 }
 
